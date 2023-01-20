9ccb18f27f4520457d66131a021b1e5ea2461041
Aurthur Musendame <aurthurmusendame@gmail.com>
2023-01-20 10:38:43 +0200

    feat(scheduling): Add multi schedule option for python schedule package and cron on linux option
    Linux users have 2 options to use cron time specicic scheduler or use just @reboot whilst windows users have cmd startup option only
    
    BREAKING CHANGE:

--- __main__.py
+++ __main__.py
@@ -1,14 +1,51 @@
 import asyncio
+import time
+import os
+import sys
 from datetime import datetime
 from engine import ResultInterface
 from logger import setup_logs_dirs, Logger
+from config import LINUX_USING_CRON_TIME_SCHEDULER
 
 setup_logs_dirs()
 logger = Logger(__name__, __file__)
 
+
+def is_linux():
+    return sys.platform == 'linux' or sys.platform.startswith('linux') or os.name == 'posix'
+
+
+def is_windows():
+    return sys.platform == 'win32' or sys.platform.startswith('win') or os.name == 'nt'
+
+
+def commence_raw():
+    asyncio.run(ResultInterface().run())
+
+
+def commence_scheuled():
+    import schedule
+
+    # run now and schedule the nest jobs
+    commence_raw()
+    schedule.every(10).minutes.do(commence_raw)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(10)
+
+
 if __name__ == '__main__':
     time_start = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     logger.log("info", f"Starting interface transmission: {time_start}")
-    asyncio.run(ResultInterface().run())
+
+    if is_linux:
+        if LINUX_USING_CRON_TIME_SCHEDULER:
+            commence_raw()
+        else:
+            commence_scheuled()
+    else:
+        commence_scheuled()
+
     time_end = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     logger.log("info", f"Finished interface transmission: {time_end}")
--- readme.md
+++ readme.md
@@ -6,3 +6,26 @@
 make sure you have installed `python 3.9.5 or higher`
 
 run `pip install -r requirements.txt`
+
+### linux setup crontab
+## option 1
+if you want to use cron specific time scheduler the set `LINUX_USING_CRON_TIME_SCHEDULER = True` and
+
+command `crontab -e` and add to the bottom of the file `*/10 * * * * /usr/bin/python path/to/script/folder`
+
+reboot and check logs
+
+## option 2
+If you want the script to manage the schecule the set `LINUX_USING_CRON_TIME_SCHEDULER = False`  and
+
+command `crontab -e` and add to the bottom of the file `@reboot /usr/bin/python path/to/script/folder`
+
+reboot and check logs
+
+
+### windows setup
+Somewhere here `C:\Users\<username>\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup`
+
+add a file with name 'hl7_interface.cmd' with this content `python path\to\your\script\folder`
+
+reboot and check logs
