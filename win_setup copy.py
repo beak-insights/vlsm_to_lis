017a66430ce288464e87b51e6e8a08b029f120b7
Aurthur Musendame <aurthurmusendame@gmail.com>
2023-01-20 14:19:24 +0200

    win startup trial 2

--- win_setup copy.py
+++ win_setup copy.py
@@ -7,26 +7,15 @@ user_path = os.path.expanduser('~')
 sartup_path = os.path.join(
     user_path, 'AppData', 'Roaming', 'Microsoft', 'Windows', 'Start Menu', 'Programs', 'Startup')
 
-sartup_file = os.path.join(BASE_DIR, "run_interface.cmd")
-silencer_file = os.path.join(sartup_path, "hl7_silencer.cmd")
+sartup_file = os.path.join(sartup_path, "hl7_senaite.bat")
 
 
 def init():
-    # create the startup file
     with open(sartup_file, "w+") as out_file:
         out_file.write(f"python {BASE_DIR}")
 
-    # create another file to run the startup script in bg
-    with open(silencer_file, "w+") as silencer:
-        file_data = f"""
-        @echo off
-        start {sartup_file}
-        exit
-        """
-        silencer.write(file_data)
-
     # reboot windows machine
-    # os.system("shutdown /r /t 1")  # os.system("shutdown -t 0 -r -f")
+    os.system("shutdown /r /t 1")  # os.system("shutdown -t 0 -r -f")
 
 
 if __name__ == '__main__':
